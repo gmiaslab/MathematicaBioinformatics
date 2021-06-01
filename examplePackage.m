5ed49a953627154074d344613bc672fd366609a8
gmiaslab <gmiaslab@users.noreply.github.com>
2021-06-01 18:11:33 -0400

    Mathematica 12.3.0
    Updated for Mathematica 12.3.0 and compatibility for various databases

--- examplePackage.m
+++ examplePackage.m
@@ -24,7 +24,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.*)
 
-BeginPackage["examplePackage`",{"ANOVA`","ErrorBarPlots`"}]
+BeginPackage["examplePackage`",{"ANOVA`","HierarchicalClustering`"}]
 
 exampleWolframLanguageFunction::usage="exampleWolframLanguageFunction is our first function"
 
